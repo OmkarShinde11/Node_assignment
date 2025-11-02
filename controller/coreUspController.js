42f2720c91534e8340751f6d8b72ed21fa2c3d8f
omkar.shinde <omkar.shinde@routemobile.com>
2025-11-03 02:21:52 +0530

    comment removed

--- controller/coreUspController.js
+++ controller/coreUspController.js
@@ -22,93 +22,13 @@ const upolod=multer({
 
 const uploadPhoto=upolod.single('Image');
 
-// const getUspCores=catchAsync(async (req,res,next)=>{
-//     const coreUsp=await coreUspModel.find();
-//     res.status(200).json({
-//         status:'Success',
-//         message:'Core Usp retrived Successfully',
-//         coreUsp,
-//     })
-// });
-
 const getUspCores=getData(coreUspModel);
 
-
-// const createCoreUsp=catchAsync(async (req,res,next)=>{
-//     console.log(req.body);
-//     console.log(req.file);
-//     const data={...req.body};
-//     data.icon=req?.file ? `${req.protocol}://${req.get('host')}/upload/image/UspIcons/${req.file.filename}` : null;
-//     console.log(data);
-//     const newData=await coreUspModel.create(data);
-//     res.status(200).json({
-//         status:'Success',
-//         message:'New coreUsp Created Successfully',
-//         newData,
-//     })
-// });
 const createCoreUsp=createData(coreUspModel,'UspIcons');
-
-// const updateCoreUsp=catchAsync(async(req,res,next)=>{
-//     const id=req.params.id;
-//     let data={...req.body};
-//     let oldData;
-//     if(req.file){
-//         console.log(req.file);
-//         let oldDoc=await coreUspModel.findById(id);
-//         if(!oldDoc) return next(new AppError(404,`There is no such Core Usp with ${id} id`));
-//         oldData=oldDoc.icon;
-//         data.icon=`${req.protocol}://${req.get('host')}/upload/image/UspIcons/${req.file.filename}`;
-//     }
-//     console.log(data);
-//     const updatedCoreUsp=await coreUspModel.findByIdAndUpdate(id,data,{new:true,runValidators:true});
-//     if(!updatedCoreUsp) return next(new AppError(404,`There is no such Core Usp with ${id} id`));
-
-//     res.status(200).json({
-//         status:'Success',
-//         message:'Core Usp Updated Successfully',
-//         updatedCoreUsp
-//     });
-
-//     // This Operation runs in background to clear and maintain disk storage.
-//     if(oldData!==undefined){
-//         backgroundOperation(oldData,'UspIcons');
-//     }
-// });
 
 const updateCoreUsp=updateData(coreUspModel,'UspIcons',true);
 
-
-// const getSingleCoreUsp=catchAsync(async(req,res,next)=>{
-//     const id=req.params.id;
-//     const coreUspDetails=await coreUspModel.findById(id);
-//     if(!coreUspDetails)return next(new AppError(404,`There is no such Core Usp with ${id} id`));
-//     res.status(200).json({
-//         status:'Success',
-//         message:'Core Usp retrived Successfully',
-//         coreUspDetails,
-//     })
-// });
-
 const getSingleCoreUsp=getSingleData(coreUspModel);
-
-// const deleteCoreUsp=catchAsync(async (req,res,next)=>{
-//     const id=req.params.id;
-//     let oldData=await coreUspModel.findById(id);
-//     let oldImage=oldData.icon;
-//     const doc=await coreUspModel.findByIdAndDelete(id);
-//     if(!doc){
-//         return next(new AppError(404,`There is no such Core Usp with ${id}`));
-//     }
-//     res.status(200).json({
-//         status:'Success',
-//         message:'Core Usp Deleted Successfully',
-//     });
-
-//     if(oldImage!==undefined){
-//         backgroundOperation(oldImage,'UspIcons');
-//     }
-// });
 
 const deleteCoreUsp=deleteData(coreUspModel,'UspIcons',true);
 const reorderCoreUsp=reorderData(coreUspModel);
--- controller/faqController.js
+++ controller/faqController.js
@@ -6,55 +6,11 @@ const multerFilter = require('../utils/multerFilter');
 const cretaeMulterStorage = require('../utils/multerStorage');
 const { getSingleData, updateData, deleteData, getData, createData, reorderData } = require('./factoryHandler');
 
-
-// const cretaeFaq=catchAsync(async(req,res,next)=>{
-//     let newfaq=await faqModel.create(req.body);
-//     res.status(200).json({
-//         status:'Success',
-//         message:'FAQ created successfully',
-//         newfaq,
-//     });
-// });
 const cretaeFaq=createData(faqModel);
-
-// const getFaq=catchAsync(async(req,res,next)=>{
-//     let faqData=await faqModel.find();
-//     res.status(200).json({
-//         status:'Success',
-//         message:'FAQ reterived Successfully',
-//         faqData,
-//     })
-// });
 
 const getFaq=getData(faqModel);
 
-// const getSingleFaq=catchAsync(async(req,res,next)=>{
-//     const id=req.params.id;
-//     let faqDetails=await faqModel.findById(id);
-//     if(!faqDetails){
-//         return next(new AppError(404,`There is no faq data with ${id}`));
-//     };
-//     res.status(200).json({
-//         status:'Success',
-//         message:'FAQ reterived successfully',
-//         faqDetails,
-//     })
-// });
-
 const getSingleFaq=getSingleData(faqModel);
-
-// const updateFaq=catchAsync(async(req,res,next)=>{
-//     const id=req.params.id;
-//     let updatedData=await faqModel.findByIdAndUpdate(id,req.body,{new:true,runValidators:true});
-//     if(!updatedData){
-//         return next(new AppError(404,`There is no faq data with ${id}`));
-//     };
-//     res.status(200).json({
-//         status:'Success',
-//         message:'FAQ Updated successfully',
-//         updatedData,
-//     })
-// });
 
 const multerStorage=cretaeMulterStorage('test','test');
 
@@ -66,17 +22,6 @@ const upolod=multer({
 const uploadPhoto=upolod.none();
 const updateFaq=updateData(faqModel);
 
-// const deleteFaq=catchAsync(async(req,res,next)=>{
-//     const id=req.params.id;
-//     let data=await faqModel.findByIdAndDelete(id);
-//     if(!data){
-//         return next(new AppError(404,`There is no faq data with ${id}`));
-//     };
-//     res.status(200).json({
-//         status:'Success',
-//         message:'FAQ Deleted successfully',
-//     })
-// });
 const deleteFaq=deleteData(faqModel);
 
 const reorderFaq=reorderData(faqModel);
